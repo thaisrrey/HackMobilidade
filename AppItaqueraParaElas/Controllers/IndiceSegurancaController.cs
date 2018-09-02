24f527c5831c9e197114f2f0934a7b5c835ad825
thaisrrey <32514264+thaisrrey@users.noreply.github.com>
2018-09-02 18:05:42 -0300

    Finalizado

---
+++ AppItaqueraParaElas/Controllers/IndiceSegurancaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppItaqueraParaElas.Models;
+
+namespace AppItaqueraParaElas.Controllers
+{
+    public class IndiceSegurancaController : Controller
+    {
+        
+        // GET: IndiceSeguranca
+        public ActionResult Index()
+        {       
+            return View();
+        }
+    }
+}
---
+++ AppItaqueraParaElas/Controllers/MapaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppItaqueraParaElas.Controllers
+{
+    public class MapaController : Controller
+    {
+        // GET: Mapa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
